9085d10954b04adb5d182f306cbd99f79a9992e9
jlange <jlange54@gmx.de>
2023-12-28 16:10:36 +0100

    Add class for various filter methods

---
+++ src/main/filter/Filter.java
@@ -0,0 +1,20 @@
+package main.filter;
+
+import main.java.objects.Airport;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Filter {
+    public static List<Airport> filterByRunwayLength (int minLength, int maxLength, List<Airport> airportList) {
+        return airportList.stream().filter(p -> p.getLongestRunway() >= minLength && p.getLongestRunway() <= maxLength).collect(Collectors.toList());
+    }
+
+    public static Airport filterByIcaoCode (String icaoCode, List<Airport> airportList) {
+        return airportList.stream().filter(p -> p.getIcaoCode().equals(icaoCode)).findAny().orElse(null);
+    }
+
+    public static List<Airport> filterByCountry (String country, List<Airport> airportList) {
+        return airportList.stream().filter(p -> p.getCountryPrefix().getCountry().equals(country)).collect(Collectors.toList());
+    }
+}
