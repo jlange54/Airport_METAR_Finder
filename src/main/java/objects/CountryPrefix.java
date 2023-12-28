e5f96452132c865b02e1ca22d9fd1f7e1d291c12
jlange <jlange54@gmx.de>
2023-12-28 16:08:19 +0100

    Add class for CountryPrefix object

---
+++ src/main/java/objects/CountryPrefix.java
@@ -0,0 +1,35 @@
+package main.java.objects;
+
+public class CountryPrefix {
+    private String prefix;
+    private String country;
+
+    public CountryPrefix(String prefix, String country) {
+        this.prefix = prefix;
+        this.country = country;
+    }
+
+    public String getPrefix() {
+        return prefix;
+    }
+
+    public void setPrefix(String prefix) {
+        this.prefix = prefix;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    @Override
+    public String toString() {
+        return "CountryPrefix{" +
+                "prefix='" + prefix + '\'' +
+                ", country='" + country + '\'' +
+                '}';
+    }
+}
