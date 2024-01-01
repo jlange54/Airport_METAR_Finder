00a2504b45fe7c8d0d3671b8b1741e81ce7230b5
jlange <jlange54@gmx.de>
2024-01-01 22:49:06 +0100

    Add new dynamic label and ListView

--- src/main/java/frontend/StartController.java
+++ src/main/java/frontend/StartController.java
@@ -2,9 +2,14 @@ package frontend;
 
 import backend.AirportList;
 import backend.objects.Airport;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
+import javafx.util.Callback;
 
 import java.io.IOException;
 import java.net.URL;
@@ -14,7 +19,9 @@ import java.util.ResourceBundle;
 public class StartController implements Initializable {
 
     @FXML
-    private ListView<String> airportListView;
+    private Label currentIcaoSelection;
+    @FXML
+    private ListView<Airport> airportListView = new ListView<>();
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -26,12 +33,25 @@ public class StartController implements Initializable {
         }
 
         for (Airport airport : airportList) {
-            airportListView.getItems().add(airport.getName());
-            airportListView.getItems().add("Hullo?");
+            airportListView.getItems().add(airport);
         }
+        airportListView.setCellFactory(param -> new ListCell<>() {
+            @Override
+            protected void updateItem(Airport airport, boolean empty) {
+                super.updateItem(airport, empty);
+                if (empty || airport == null) {
+                    setText(null);
+                } else {
+                    setText(airport.getName());
+                }
+            }
+        });
+
+       airportListView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Airport>() {
+           @Override
+           public void changed(ObservableValue<? extends Airport> observable, Airport oldValue, Airport newValue) {
+               currentIcaoSelection.setText(airportListView.getSelectionModel().getSelectedItem().getIcaoCode());
+           }
+       });
     }
-
-
-
-
 }
