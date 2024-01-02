728c424e4abe417e9a502fe3a8bbfa5b4d27cfc7
jlange54 <127547473+jlange54@users.noreply.github.com>
2024-01-02 22:43:52 +0100

    Update Config.java

--- src/main/java/backend/Config.java
+++ src/main/java/backend/Config.java
@@ -4,7 +4,4 @@ public class Config {
 
     public static String airports_txt  = "/data/airports.txt";
     public static String countryPrefixes_csv  = "/data/countryPrefixes.csv";
-
-    public static String avwxToken = "E2tKVFVzacpyxegrz3K5lFhgU-BnqB7K6X8Ghjkc5QQ";
-
 }
