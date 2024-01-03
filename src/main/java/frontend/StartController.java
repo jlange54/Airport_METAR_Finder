29df550d7663603b12438868c087b5b582804672
jlange <jlange54@gmx.de>
2024-01-03 10:03:00 +0100

    Add new functions

--- src/main/java/frontend/StartController.java
+++ src/main/java/frontend/StartController.java
@@ -1,7 +1,10 @@
 package frontend;
 
 import backend.AirportList;
+import backend.AvwxApi;
+import backend.MetarParser;
 import backend.objects.Airport;
+import backend.objects.metar.Metar;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
@@ -9,7 +12,8 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
-import javafx.util.Callback;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.GridPane;
 
 import java.io.IOException;
 import java.net.URL;
@@ -21,13 +25,30 @@ public class StartController implements Initializable {
     @FXML
     private Label currentIcaoSelection;
     @FXML
+    private Label countryLabel;
+    @FXML
+    private Label elevationLabel;
+    @FXML
+    private Label longestRunwayLabel;
+    @FXML
+    private Label generalAirportInformationLabel;
+    @FXML
+    private GridPane generalAirportInformationGridPane;
+    @FXML
     private ListView<Airport> airportListView = new ListView<>();
+    @FXML
+    private TextArea currentWeatherTextArea;
 
     private String currentIcao;
+    private Metar currentMetar;
+    private boolean metarAvailable = false;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        List<Airport> airportList = null;
+        generalAirportInformationLabel.setVisible(false);
+        generalAirportInformationGridPane.setVisible(false);
+
+        List<Airport> airportList;
         try {
             airportList = AirportList.generate();
         } catch (IOException e) {
@@ -53,7 +74,39 @@ public class StartController implements Initializable {
            @Override
            public void changed(ObservableValue<? extends Airport> observable, Airport oldValue, Airport newValue) {
                currentIcao = airportListView.getSelectionModel().getSelectedItem().getIcaoCode();
+
                currentIcaoSelection.setText(currentIcao);
+               if (airportListView.getSelectionModel().getSelectedItem().getCountryPrefix().getCountry().equals("noCountry")) {
+                   countryLabel.setVisible(false);
+               } else {
+                   countryLabel.setText(airportListView.getSelectionModel().getSelectedItem().getCountryPrefix().getCountry());
+                   countryLabel.setVisible(true);
+               }
+               elevationLabel.setText(airportListView.getSelectionModel().getSelectedItem().getElevation() + " feet");
+               longestRunwayLabel.setText(airportListView.getSelectionModel().getSelectedItem().getLongestRunway() + " feet");
+
+               generalAirportInformationLabel.setVisible(true);
+               generalAirportInformationGridPane.setVisible(true);
+
+               try {
+                   if (AvwxApi.getMetarForStation(currentIcao) != null) {
+                       currentMetar = MetarParser.parse(AvwxApi.getMetarForStation(currentIcao));
+                       System.out.println(currentMetar);
+                       metarAvailable = true;
+                   }
+               } catch (IOException e) {
+                   throw new RuntimeException(e);
+               }
+
+               if (metarAvailable) {
+                   currentWeatherTextArea.setText(
+                           "The weather at " + airportListView.getSelectionModel().getSelectedItem().getName() + " is : " +
+                                   currentMetar.getWxCode().getFirst().getValue() + " the temperature is: " + currentMetar.getTemperature() +
+                                   "Wind is coming from " + currentMetar.getWindDirection() + " at " + currentMetar.getWindSpeed() + currentMetar.getUnits().getWindSpeed()
+                   );
+               } else {
+                   currentWeatherTextArea.setText("No METAR available for " + airportListView.getSelectionModel().getSelectedItem().getName());
+               }
            }
        });
     }
