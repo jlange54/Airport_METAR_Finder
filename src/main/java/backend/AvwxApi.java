1c6c7ce35c6c9587793b5b5e3e578db56a2422e5
jlange <jlange54@gmx.de>
2024-01-03 10:03:55 +0100

    Add check if API response is good

--- src/main/java/backend/AvwxApi.java
+++ src/main/java/backend/AvwxApi.java
@@ -8,15 +8,22 @@ import javax.ws.rs.core.MediaType;
 
 public class AvwxApi {
 
-    public static void getMetarForStation (String icao) {
+    public static String  getMetarForStation (String icao) {
         Client client = ClientBuilder.newClient();
         Response response = client.target("https://avwx.rest/api/metar/" + icao)
                         .request(MediaType.TEXT_PLAIN_TYPE)
                         .header("Authorization", Config.avwxToken)
                         .get();
 
-        System.out.println("status: " + response.getStatus());
-        System.out.println("headers: " + response.getHeaders());
-        System.out.println("body:" + response.readEntity(String.class));
+//        System.out.println(response.readEntity(String.class));
+        if (response.getStatus() == 200) {
+            return response.readEntity(String.class);
+        } else if (response.getStatus() == 204) {
+            System.out.println(response.getStatus());
+            return null;
+        } else {
+            new Exception("response.getStatus()");
+            return null;
+        }
     }
 }
