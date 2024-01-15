e324b8fae591c4832a379fe205409ca4890d05ad
jlange <jlange54@gmx.de>
2024-01-15 12:27:43 +0100

    Changed to get API key from System Env Var

---
+++ src/main/java/backend/Config.java
@@ -0,0 +1,7 @@
+package backend;
+
+public class Config {
+    public static String airports_txt = "/data/airports.txt";
+    public static String countryPrefixes_csv = "/data/countryPrefixes.csv";
+    public static String avwxToken = System.getenv("AirportMetarFinderAvwxApiToken");
+}
